1b71d4a366bf2bbef8c48e207d5fab50b90ac600
Paul Javid <paul.javid@gmail.com>
2011-02-10 17:27:57 -0800

    fixed errors for pages_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
   
   get "pages/about"
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
