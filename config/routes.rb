e47c1bdf7882042d39bc540760252dd34b6f3611
Paul Javid <paul.javid@gmail.com>
2011-02-11 15:06:53 -0800

    added layout links with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
 
-  get "pages/contact"
   
-  get "pages/about"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -60,7 +60,24 @@ end
       get 'home'
       response.body.should_not =~ /<body>\s*<\/body>/
     end
-    
   end
-
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", 
+                                    :content => "#{@base_title} | Help")
+    end
+    
+    it "should have a non-blank body" do
+      get 'help'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+  end
+  
 end
